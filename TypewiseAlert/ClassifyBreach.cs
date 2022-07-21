e4e728e79e2e0b77c38d17b5e88e5b6895eb24d7
srvnk <105205717+srvnk@users.noreply.github.com>
2022-07-21 22:23:44 +0530

    Update ClassifyBreach.cs

--- TypewiseAlert/ClassifyBreach.cs
+++ TypewiseAlert/ClassifyBreach.cs
@@ -18,37 +18,5 @@ namespace TypewiseAlert
 
             return InferBreach.inferBreach(temperatureInC, cooling.LowerLimit, cooling.Upperlimit);
         }
-        
-        public List<CoolingTypeModel> GetCoolingTypeData()
-        {
-            try
-            {
-                List<CoolingTypeModel> coolingTypeList = new List<CoolingTypeModel>();
-                CoolingTypeModel coolingType = null;
-
-                XmlDocument xmlDocument = new XmlDocument();
-                xmlDocument.Load(string.Format("{0}/{1}", AppDomain.CurrentDomain.BaseDirectory, "CoolingTypeFile.xml"));
-
-                XmlNodeList nodeList = xmlDocument.DocumentElement.SelectNodes("/CoolingTypeList/CoolingType");
-
-                if (nodeList != null && nodeList.Count > 0)
-                {
-                    foreach (XmlNode xmlNode in nodeList)
-                    {
-                        coolingType = new CoolingTypeModel();
-                        coolingType.CoolingType = Convert.ToInt32(xmlNode.SelectSingleNode("CoolingType").InnerText);
-                        coolingType.LowerLimit = Convert.ToDouble(xmlNode.SelectSingleNode("LowerLimit").InnerText);
-                        coolingType.Upperlimit = Convert.ToDouble(xmlNode.SelectSingleNode("Upperlimit").InnerText);
-
-                        coolingTypeList.Add(coolingType);
-                    }
-                }
-                return coolingTypeList;
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
     }
 }
