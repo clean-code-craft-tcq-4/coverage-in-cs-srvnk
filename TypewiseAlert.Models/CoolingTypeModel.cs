f020854d163f5b9009966e55adeb8fb5ac1b1770
srvnk <105205717+srvnk@users.noreply.github.com>
2022-07-21 20:42:12 +0530

    Create CoolingTypeModel.cs

---
+++ TypewiseAlert.Models/CoolingTypeModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypewiseAlert.Models
+{
+    public class CoolingTypeModel
+    {
+        public int CoolingType { get; set; }
+        public double LowerLimit { get; set; }
+        public double Upperlimit { get; set; }
+    }
+}
