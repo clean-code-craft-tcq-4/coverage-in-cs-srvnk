c8f3f492c07d0426ed769d8ca59edb95387ab1ca
srvnk <105205717+srvnk@users.noreply.github.com>
2022-07-21 22:17:45 +0530

    Update ClassifyBreach.cs

--- TypewiseAlert/ClassifyBreach.cs
+++ TypewiseAlert/ClassifyBreach.cs
@@ -7,7 +7,7 @@ namespace TypewiseAlert
 {
     public class ClassifyBreach
     {
-        public static BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC)
+        public BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC)
         {
             List<CoolingTypeModel> coolingList = new List<CoolingTypeModel>();
             XmlData xmlData = new XmlData();
@@ -17,6 +17,38 @@ namespace TypewiseAlert
             CoolingTypeModel cooling = coolingList.Where(t => t.CoolingType == Convert.ToInt32(coolingType)).FirstOrDefault();
 
             return InferBreach.inferBreach(temperatureInC, cooling.LowerLimit, cooling.Upperlimit);
+        }
+        
+        public List<CoolingTypeModel> GetCoolingTypeData()
+        {
+            try
+            {
+                List<CoolingTypeModel> coolingTypeList = new List<CoolingTypeModel>();
+                CoolingTypeModel coolingType = null;
+
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(string.Format("{0}/{1}", AppDomain.CurrentDomain.BaseDirectory, "CoolingTypeFile.xml"));
+
+                XmlNodeList nodeList = xmlDocument.DocumentElement.SelectNodes("/CoolingTypeList/CoolingType");
+
+                if (nodeList != null && nodeList.Count > 0)
+                {
+                    foreach (XmlNode xmlNode in nodeList)
+                    {
+                        coolingType = new CoolingTypeModel();
+                        coolingType.CoolingType = Convert.ToInt32(xmlNode.SelectSingleNode("CoolingType").InnerText);
+                        coolingType.LowerLimit = Convert.ToDouble(xmlNode.SelectSingleNode("LowerLimit").InnerText);
+                        coolingType.Upperlimit = Convert.ToDouble(xmlNode.SelectSingleNode("Upperlimit").InnerText);
+
+                        coolingTypeList.Add(coolingType);
+                    }
+                }
+                return coolingTypeList;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
     }
 }
