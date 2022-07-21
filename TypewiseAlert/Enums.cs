cff5c979cfb639ee0e9330dca516acba5f9c90b2
srvnk <105205717+srvnk@users.noreply.github.com>
2022-07-21 20:37:14 +0530

    Create Enums.cs

---
+++ TypewiseAlert/Enums.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypewiseAlert
+{
+    public class Enums
+    {
+        public enum BreachType
+        {
+            TOO_LOW = 0,
+            NORMAL = 1,
+            TOO_HIGH = 2
+        };
+        public enum CoolingType
+        {
+            PASSIVE_COOLING = 0,
+            MED_ACTIVE_COOLING = 1,
+            HI_ACTIVE_COOLING = 2
+        };
+        public enum AlertTarget
+        {
+            TO_CONTROLLER,
+            TO_EMAIL
+        };
+    }
+}
