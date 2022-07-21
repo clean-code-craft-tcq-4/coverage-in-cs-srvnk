3e37d0795104daee382aba46e142843b366c6e35
srvnk <105205717+srvnk@users.noreply.github.com>
2022-07-21 20:42:59 +0530

    Create BreachInferTest.cs

---
+++ TypewiseAlert.Test/BreachInferTest.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TypewiseAlert.Enums;
+
+namespace TypewiseAlert.Test
+{
+    class BreachInferTest
+    {
+        public void InfersBreachAsPerLimits()
+        {
+            Assert.True(InferBreach.inferBreach(12, 20, 30) == BreachType.TOO_LOW);
+            Assert.True(InferBreach.inferBreach(0, 20, 30) == BreachType.TOO_LOW);
+            Assert.True(InferBreach.inferBreach(6, 20, 30) == BreachType.TOO_LOW);
+
+            Assert.True(InferBreach.inferBreach(25, 20, 30) == BreachType.NORMAL);
+            Assert.True(InferBreach.inferBreach(28, 20, 30) == BreachType.NORMAL);
+            Assert.True(InferBreach.inferBreach(22, 20, 30) == BreachType.NORMAL);
+
+            Assert.True(InferBreach.inferBreach(40, 20, 30) == BreachType.TOO_HIGH);
+            Assert.True(InferBreach.inferBreach(50, 20, 30) == BreachType.TOO_HIGH);
+            Assert.True(InferBreach.inferBreach(100, 20, 30) == BreachType.TOO_HIGH);
+        }
+    }
+}
