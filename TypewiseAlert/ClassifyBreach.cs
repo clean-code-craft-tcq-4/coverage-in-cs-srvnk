5c7fb90c3fa831abf03172673ad68be76b609d49
srvnk <105205717+srvnk@users.noreply.github.com>
2022-07-21 20:56:46 +0530

    Update ClassifyBreach.cs

--- TypewiseAlert/ClassifyBreach.cs
+++ TypewiseAlert/ClassifyBreach.cs
@@ -1,10 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TypewiseAlert.Manager;
-using TypewiseAlert.Models;
 using static TypewiseAlert.Enums;
 
 namespace TypewiseAlert
