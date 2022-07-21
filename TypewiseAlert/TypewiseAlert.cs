3831dd50ccb5163a30d3a2f7274af03aafed6ef1
srvnk <105205717+srvnk@users.noreply.github.com>
2022-07-21 21:00:20 +0530

    Update TypewiseAlert.cs

--- TypewiseAlert/TypewiseAlert.cs
+++ TypewiseAlert/TypewiseAlert.cs
@@ -1,10 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TypewiseAlert.Manager;
-using TypewiseAlert.Models;
 using static TypewiseAlert.Enums;
 
 namespace TypewiseAlert
