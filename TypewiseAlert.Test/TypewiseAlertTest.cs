69ac5cc13741a1ee092f8e4cc6d049c1b2c92078
srvnk <105205717+srvnk@users.noreply.github.com>
2022-07-21 20:44:50 +0530

    Update TypewiseAlertTest.cs

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -1,15 +1,59 @@
 using System;
-using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TypewiseAlert.Enums;
 
 namespace TypewiseAlert.Test
 {
-  public class TypewiseAlertTest
-  {
-    [Fact]
-    public void InfersBreachAsPerLimits()
+    public class TypewiseAlertTest
     {
-      Assert.True(TypewiseAlert.inferBreach(12, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_LOW);
+        public void TypewiseTest()
+        {
+            TypewiseAlert.BatteryCharacter batteryCharacter;
+            batteryCharacter.coolingType = CoolingType.PASSIVE_COOLING;
+            batteryCharacter.brand = "BatteryBrand";
+
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, 0));
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, 25));
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, 100));
+
+            batteryCharacter.coolingType = CoolingType.MED_ACTIVE_COOLING;
+            batteryCharacter.brand = "BatteryBrand";
+
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, 0));
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, 25));
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, 100));
+
+            batteryCharacter.coolingType = CoolingType.HI_ACTIVE_COOLING;
+            batteryCharacter.brand = "BatteryBrand";
+
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, 0));
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, 25));
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, 100));
+
+            batteryCharacter.coolingType = CoolingType.PASSIVE_COOLING;
+            batteryCharacter.brand = "BatteryBrand";
+
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 0));
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 25));
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 100));
+
+            batteryCharacter.coolingType = CoolingType.MED_ACTIVE_COOLING;
+            batteryCharacter.brand = "BatteryBrand";
+
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 0));
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 25));
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 100));
+
+            batteryCharacter.coolingType = CoolingType.HI_ACTIVE_COOLING;
+            batteryCharacter.brand = "BatteryBrand";
+
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 0));
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 25));
+            Assert.True(TypewiseAlert.checkAndAlert(AlertTarget.TO_EMAIL, batteryCharacter, 100));
+
+        }
     }
-  }
 }
